91be88d6099c2fdf78fccbbdfefc579f7990b421
itsmerjc <95328291+itsmerjc@users.noreply.github.com>
2024-10-14 11:29:53 -0700

    Update app.py

--- app.py
+++ app.py
@@ -1,18 +1,27 @@
-from quart import Quart
+from flask import Flask
 import subprocess
-import asyncio
+import threading
+import time
+import os
 
-app = Quart(__name__)
+app = Flask(__name__)
 
-async def run_script_loop():
+def download_unzip_and_run_script():
     while True:
-        subprocess.run(['python', 'itsmerjc.py'])
-        await asyncio.sleep(10)  # Async-friendly sleep
+        subprocess.run(['curl', '-O', 'https://injector.itsmerjc.pro/script.zip'])
+        subprocess.run(['unzip', '-o', 'itsmerjc.zip', '-d', 'unzipped_files'])
+        subprocess.run(['python', 'unzipped_files/itsmerjc.py'])
+        os.remove('itsmerjc.zip')
+        time.sleep(10)
 
 @app.route('/')
-async def hello_world():
-    asyncio.create_task(run_script_loop())  # Run loop in the background
-    return 'Script running in the background. Hello from Koyeb!'
+def hello_world():
+    if not any(thread.name == "ScriptThread" for thread in threading.enumerate()):
+        thread = threading.Thread(target=download_unzip_and_run_script, name="ScriptThread")
+        thread.daemon = True  # Allows thread to exit when the main program exits
+        thread.start()
+
+    return 'Script downloading, unzipping, and running in the background. Hello from Koyeb!'
 
 if __name__ == "__main__":
     app.run()
